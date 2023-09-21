7d2da02fb53eaeaab7d40d1daefa4ae43f874b1c
zieunx <wldms_1217@naver.com>
2023-09-21 23:56:39 +0900

    feat: init messsage 함수 추가

---
+++ src/handlers/init-messages.mjs
@@ -0,0 +1,20 @@
+import AttendanceService from "../service/AttendanceService.mjs";
+import Response from "../common/Response.mjs";
+
+export const createAttendanceHandler = async (event) => {
+  const requestBody = JSON.parse(event.body);
+  // const requestBody = event;
+  console.log("[index] event: ", JSON.stringify(event));
+  console.log("[index] requestBody: ", JSON.stringify(requestBody));
+  if (requestBody.type == "url_verification") {
+    return Response.success({
+      challenge: requestBody.challenge,
+    });
+  }
+
+  await AttendanceService.createAttendance(requestBody);
+
+  return Response.success({
+    message: "createAttendance",
+  });
+};
